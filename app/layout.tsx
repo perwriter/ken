dd6a7c86d23cb79e33a50670758e192ad5a43376
perwriters <perwriters@gmail.com>
2025-02-03 17:50:41 +0300

    favi

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Resume",
-  description: "Kenneth",
+  title: "Kenneth Webber",
+  description: "Kenneth Webber Different Resumes Designs",
 };
 
 export default function RootLayout({
